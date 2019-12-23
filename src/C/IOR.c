d1c115efcf93513231388560159c50c0238658ed
Be_a_panda <huayusheng0912@gmail.com>
2019-12-22 18:17:43 -0600

    debug

--- src/C/IOR.c
+++ src/C/IOR.c
@@ -3019,7 +3019,7 @@ WriteOrRead_ec(IOR_param_t *test,
         ec_timers = (double *)malloc(sizeof(double) * total_stripe_num);
         threads = (pthread_t *)malloc(sizeof(pthread_t) * total_stripe_num);
         ec_read_args = (ec_read_thread_args *)malloc(sizeof(ec_read_thread_args) * total_stripe_num);
-        tranferDone = (int *)malloc(sizeof(int) * total_stripe_num);
+        tranferDone = (int *)calloc(sizeof(int) * total_stripe_num);
 
         ec_data = (char **)malloc(sizeof(char *) * k);
         ec_coding = (char **)malloc(sizeof(char *) * m);
