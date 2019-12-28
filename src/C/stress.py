7701ba3b30831230decb4e624b7f89ac956968c7
Be_a_panda <huayusheng0912@gmail.com>
2019-12-28 11:03:59 -0600

    add stress.py

---
+++ src/C/stress.py
@@ -0,0 +1,49 @@
+import os
+import random
+import numpy as np
+import time
+import tqdm
+
+nodeNum = 8
+
+P = 0.2
+np.random.seed(0)
+p = np.array([p, 1-p])
+p = p.ravel()
+
+T = 60
+
+S =     ["1",
+         "2",
+         "3",
+         "4",
+         "5",
+         "6"]
+
+nodeList = ["cd /data/ost0;",
+            "cd /data/ost1;",
+            "cd /data/ost2;",
+            "cd /data/ost3;",
+            "cd /data/ost4;",
+            "cd /data/ost5;",
+            "cd /data/ost6;",
+            "cd /data/ost7;"]
+
+numIterations = 100;
+
+print "stress generator start...\n"
+
+for it in trange(numIterations):
+    for i in range(nodeNum):
+        is_straggler = np.random.choice([1, 0], p)
+        if is_straggler:
+
+            directory = nodeList[i]
+            hdd = hddList[random.randint(0, len(hddList)-1)]
+            time = str(random.randint(0, T))
+            stressCmd = "nohup stress --hdd " + hdd +" --timeout "+ time + "&"
+            cmd = directory + " " + stressCmd
+            print "cmd is "+cmd
+            print "ost" + i + " is straggler, degree is " + hdd + ", time is " + time
+            os.system(cmd)
+    time.sleep(T)
