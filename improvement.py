0b408fe62cf1b511198362202be9d69a1b7dda17
Be_a_panda <huayusheng0912@gmail.com>
2019-12-28 16:01:13 -0600

    add improvement.py

---
+++ improvement.py
@@ -0,0 +1,10 @@
+import os
+import random
+import numpy as np
+import time
+from tqdm import tqdm
+
+os.system("sudo ./refresh.sh")
+os.system("mpiexec -n 1 src/C/IOR -f read_8n_3")
+os.system("sudo ./refresh.sh")
+os.system("mpiexec -n 1 src/C/IOR -f read_8n_3g_ec")
