b8a9c8c30bb973d15688687dc35a35e1e87b22ae
Be_a_panda <huayusheng0912@gmail.com>
2019-12-28 16:05:03 -0600

    debug

--- improvement.py
+++ improvement.py
@@ -5,6 +5,6 @@ import time
 from tqdm import tqdm
 
 os.system("sudo ./refresh.sh")
-os.system("mpiexec -n 1 src/C/IOR -f read_8n_3")
+os.system("mpiexec -n 1 src/C/IOR -f read_8n_3g")
 os.system("sudo ./refresh.sh")
 os.system("mpiexec -n 1 src/C/IOR -f read_8n_3g_ec")
