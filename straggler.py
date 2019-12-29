a7db0cce4f57eabd72e732385afc0186c3a45dc0
Be_a_panda <huayusheng0912@gmail.com>
2019-12-28 18:32:33 -0600

    debug

--- straggler.py
+++ straggler.py
@@ -47,6 +47,7 @@ for it in tqdm(range(numIterations)):
     lines = f.stdout.readlines()
     temp = []
     for line in lines:
+        print(line[0:6])
         if line[0:6] == "b'read":
             line=line[2:-3]
             print(line)
