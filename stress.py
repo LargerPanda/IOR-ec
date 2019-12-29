1fab19a6e83bc652f59c2a81689a48279ac8f86c
Be_a_panda <huayusheng0912@gmail.com>
2019-12-29 13:17:04 -0600

    modify stress.py

--- stress.py
+++ stress.py
@@ -3,23 +3,17 @@ import random
 import numpy as np
 import time
 from tqdm import tqdm
+import sys
 
 nodeNum = 8
 
-P = 0.2
-np.random.seed(0)
-p = np.array([P, 1-P])
-p = p.ravel()
 
-T = 60
-
-
-S =     ["1",
-         "2",
+S =     ["2",
          "3",
          "4",
          "5",
-         "6"]
+         "6",
+         "7"]
 
 nodeList = ["cd /data/ost0;",
             "cd /data/ost1;",
@@ -30,24 +24,28 @@ nodeList = ["cd /data/ost0;",
             "cd /data/ost6;",
             "cd /data/ost7;"]
 
-numIterations = 100;
-
 print("stress generator start...\n")
 
 #for it in tqdm(range(numIterations)):
-while True:
-    sleepT = 0
-    for i in range(nodeNum):
-        is_straggler = random.random()
-        if is_straggler>=0 and is_straggler<P:
-            directory = nodeList[i]
-            hdd = S[random.randint(0, len(S)-1)]
-            stressTime = random.randint(0, T) + 60
-            stressCmd = "nohup stress --hdd " + hdd +" --timeout "+ str(stressTime) + "&"
-            cmd = directory + " " + stressCmd
-            #print("cmd is " + cmd)
-            print("ost" + str(i) + " is straggler, degree is " + hdd +
-                  ", time is " + str(stressTime))
-            sleepT = max(sleepT, stressTime)
-            os.system(cmd)
-    time.sleep(sleepT)
+def stress_generator(p, t):
+    print("stress generator with p="+p+" t="+t)
+    while True:
+        sleepT = 0
+        for i in range(nodeNum):
+            is_straggler = random.random()
+            if is_straggler >= 0 and is_straggler < p:
+                directory = nodeList[i]
+                hdd = S[random.randint(0, len(S) - 1)]
+                stressTime = random.randint(0, t)
+                stressCmd = "nohup stress --hdd " + hdd + " --timeout " + str(
+                    stressTime) + "&"
+                cmd = directory + " " + stressCmd
+                #print("cmd is " + cmd)
+                print("ost" + str(i) + " is straggler, degree is " + hdd +
+                      ", time is " + str(stressTime))
+                sleepT = max(sleepT, stressTime)
+                os.system(cmd)
+        time.sleep(sleepT)
+
+
+stress_generator(float(sys.argv[1]), int(sys.argv[2]))
