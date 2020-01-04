233ed7c3e092eaca80e0b4d82687790612e14a63
Be_a_panda <huayusheng0912@gmail.com>
2020-01-04 01:49:55 -0600

    add stress_single_random

---
+++ stress_single_randomnode.py
@@ -0,0 +1,46 @@
+import os
+import random
+import numpy as np
+import time
+from tqdm import tqdm
+from random import choice
+import sys
+
+nodeNum = 8
+
+S = ["2", "3", "4", "5", "6", "7"]
+
+nodeList = [
+    "cd /data/ost0;", "cd /data/ost1;", "cd /data/ost2;", "cd /data/ost3;",
+    "cd /data/ost4;", "cd /data/ost5;", "cd /data/ost6;", "cd /data/ost7;"
+]
+
+print("stress generator start...\n")
+
+
+#for it in tqdm(range(numIterations)):
+def stress_generator(n, s, t):
+    print("stress generator with s=" + str(s) + " t=" + str(t) + " nodenumber=" +
+          str(n))
+    while True:
+        sleepT = 0
+        i = n
+        directory = choice(nodeList)
+        hdd = S[s]
+        stressTime = t
+        stressCmd = "nohup stress --hdd " + hdd + " --timeout " + str(
+            stressTime) + "&"
+        cmd = directory + " " + stressCmd
+        #print("cmd is " + cmd)
+        print("ost" + str(i) + " is straggler, degree is " + hdd +
+              ", time is " + str(stressTime))
+        sleepT = max(sleepT, stressTime)
+        os.system(cmd)
+        time.sleep(sleepT)
+
+
+###stress_node.py ost_num s t
+###ost_num : 0-7
+###s       : 0-5
+###t       : 5 10 20 40 80
+stress_generator(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
