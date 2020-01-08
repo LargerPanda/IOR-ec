7c988e6e5ac306b98ff9668521a3a95deb449ac6
Be_a_panda <huayusheng0912@gmail.com>
2020-01-07 19:09:40 -0600

    add stress_consequence

---
+++ stress_consequent.py
@@ -0,0 +1,49 @@
+import os
+import random
+import numpy as np
+import time
+from tqdm import tqdm
+from random import choice
+import sys
+
+nodeNum = 8
+
+S = ["2", "3", "4", "5", "6", "7", "8"]
+
+nodeList = [
+    "cd /data/ost0;", "cd /data/ost1;", "cd /data/ost2;", "cd /data/ost3;",
+    "cd /data/ost4;", "cd /data/ost5;", "cd /data/ost6;", "cd /data/ost7;"
+]
+
+print("stress generator start...\n")
+
+
+#for it in tqdm(range(numIterations)):
+def stress_generator(n, s, t):
+    print("stress generator with s=" + str(s) + " t=" + str(t) +
+          " concurrent straggler =" + str(n))
+    i = 0
+    while True:
+        sleepT = 0
+        for each in range(n):
+            #directory = choice(nodeList)
+            directory = nodeList[(each+i)%8]
+            hdd = S[s]
+            stressTime = t
+            stressCmd = "nohup stress --hdd " + hdd + " --timeout " + str(
+                stressTime) + "&"
+            cmd = directory + " " + stressCmd
+            #print("cmd is " + cmd)
+            print(directory + " is straggler, degree is " + hdd + ", time is " +
+                str(stressTime))
+            sleepT = max(sleepT, stressTime)
+            os.system(cmd)
+        time.sleep(sleepT)
+        i = (i+n)%8
+
+
+###stress_node.py num s t
+###stragglers at the same time : 
+###s       : 0-6
+###t       : 5 10 20 40 80
+stress_generator(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
