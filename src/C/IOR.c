4ab7699166ad1ed647c2ca30700e3d1ecb974238
Be_a_panda <huayusheng0912@gmail.com>
2020-01-09 22:27:14 -0600

    debhg

--- src/C/IOR.c
+++ src/C/IOR.c
@@ -3882,6 +3882,8 @@ ec_collective_thread3(ec_read_thread_args *arg)
                 fprintf(stdout,"process %d:thread %d end with compute %lld\n", rank,id, pairCnt);
                 break;
             }
+
+            fprintf(stdout,"process %d:thread %d current pairCnt =  %lld\n", rank,id, pairCnt);
             
             if(pairCnt < kth_large_offset_of_stripes(k+m,k)){
                 if (method == Reed_Sol_Van || method == Reed_Sol_R6_Op)
