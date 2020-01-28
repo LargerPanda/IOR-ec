001af7027fc9bc519ba9ff2269b31d7c2a9f1121
Be_a_panda <huayusheng0912@gmail.com>
2020-01-28 02:38:36 -0600

    debug

--- src/C/IOR.c
+++ src/C/IOR.c
@@ -3139,9 +3139,10 @@ void*
 ec_parity_thread0(ec_read_thread_args *arg){
     double startTime = GetTimeStamp();
     int i;
-    int nouse = 1000000;
+    int nouse;
     for(i=0;i<parity_number[0];i++){
         IOR_Xfer_ec(arg->access, (arg->fds)[6+parity_target[0]], (arg->ec_coding)[parity_target[0]], arg->test->ec_stripe_size, arg->test, arg->offSetArray[parity_start[0]+i]);
+        nouse = 1000000;
         while(nouse--){
             ;
         }
