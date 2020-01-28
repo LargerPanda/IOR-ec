2f06c1a52092567638d8067b83adc03a5241d688
Be_a_panda <huayusheng0912@gmail.com>
2020-01-28 14:28:22 -0600

    debug

--- src/C/IOR.c
+++ src/C/IOR.c
@@ -3362,8 +3362,8 @@ ec_adaptive_thread(ec_read_thread_args *arg)
     double C_latency;
     double S_latency;
     double P_latency;
-    double C = 5;
-    double S = 4;
+    double C = 13;
+    double S = 9;
     int num_0 = 1;
     int num_1 = 1;
     int should_decode = 0;
